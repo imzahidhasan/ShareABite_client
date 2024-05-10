d55321877299a76a118a38b8d36a967ccd4c47b4
Zahid Hasan <imzahidhasan.mail@gmail.com>
2024-05-10 07:36:54 +0600

    added logo and favicon

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -14,6 +14,7 @@ const Navbar = () => {
     <NavLink to={'/manage_my_food'} className={({ isActive }) => isActive ? `text-blue-600 dark:text-blue-500 font-medium` : `font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-neutral-400 dark:hover:text-neutral-500`}>Manage My Food</NavLink>
     <NavLink to={'/my_food_request'} className={({ isActive }) => isActive ? `text-blue-600 dark:text-blue-500 font-medium` : `font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-neutral-400 dark:hover:text-neutral-500`}>My Food Request</NavLink>
 
+    
     <NavLink to={'/login'} className={({ isActive }) => isActive ? `text-blue-600 dark:text-blue-500 flex items-center gap-x-2 font-medium  sm:border-s sm:border-gray-300 sm:my-6 sm:ps-6 dark:border-neutral-700 `: `flex items-center gap-x-2 font-medium text-gray-500 hover:text-blue-600 sm:border-s sm:border-gray-300 sm:my-6 sm:ps-6 dark:border-neutral-700 dark:text-neutral-400 dark:hover:text-blue-500`} >
       <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
         <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
@@ -28,7 +29,9 @@ const Navbar = () => {
       <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white border-b border-gray-200 text-sm py-3 sm:py-0 dark:bg-neutral-800 dark:border-neutral-700">
         <nav className="relative max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8" aria-label="Global">
           <div className="flex items-center justify-between">
-            <Link to={'/'} className="flex-none text-xl font-semibold dark:text-white" href="#" aria-label="Brand">ShareABite</Link>
+            <Link to={'/'} className="flex justify-center gap-2 items-center text-xl font-semibold dark:text-white" aria-label="Brand">
+              <img src={'/logo.png'} className='size-8' alt="logo" />
+              ShareABite</Link>
             <div className="sm:hidden">
               <button type="button" className="hs-collapse-toggle size-9 flex justify-center items-center text-sm font-semibold rounded-lg border border-gray-200 text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-neutral-700 dark:hover:bg-neutral-700" data-hs-collapse="#navbar-collapse-with-animation" aria-controls="navbar-collapse-with-animation" aria-label="Toggle navigation">
                 <svg className="hs-collapse-open:hidden size-4" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
